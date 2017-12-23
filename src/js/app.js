15fc666e28c087ad7b0ffbdc17a106c894a1d172
Alexadr Khromov <4-09@bk.ru>
2017-12-24 02:07:49 +0700

    add parallax

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,13 @@
 const slider = require('./common/slider');
 const flip = require('./common/flipper');
 const fullmenu = require('./common/fullmenu');
+const parallax = require('./common/parallax');
+const parallaxScroll = require('./common/parallaxScroll');
 
 
 // slider(); // инициализируем слайдер
 
 flip();
 fullmenu();
+parallax();
+parallaxScroll();
---
+++ src/js/common/parallaxScroll.js
@@ -0,0 +1,21 @@
+(function () {
+
+    const background = document.getElementsByClassName('parallax-scroll__layer');
+
+    function moveLayers(block,windowScroll,strafeAmount) {
+        let strafe = windowScroll / -strafeAmount + '%';
+        let style = block.style;
+        style.transform = `translate3d(0,${strafe},0)`;
+    }
+
+    function parallaxScrollInit() {
+        window.addEventListener('scroll',function () {
+            let wScroll = window.pageYOffset;
+            moveLayers(background[0],wScroll,30);
+            console.log(wScroll);
+        });
+    }
+
+    module.exports = parallaxScrollInit;
+
+})();
